ae66b22555a9ed80cf9fc05cc7deb2832fc48be7
luckymore <njuluckwang@gmail.com>
2016-01-25 15:22:35 +0800

    Initial Commit

---
+++ Loyalty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Loyalty
+//
+//  Created by WangKun on 16/1/25.
+//  Copyright © 2016年 WangKun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
